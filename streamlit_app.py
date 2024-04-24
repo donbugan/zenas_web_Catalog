79ec3228b61e03451029a5c6448b112e5d877eac
Donavan <donbugan@gmail.com>
2024-04-24 14:05:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import snowflake.connector
 import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
-my_cnx = streamlit.secrets["snowflake"]
+my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog my_cur.execute("select color_or_style from catalog_for_website") my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
@@ -20,6 +20,4 @@ df2 = my_cur.fetchone()
 streamlit.image( df2[0],
 width=400,
 caption= product_caption )
-streamlit.write('Price: ', df2[1]) 
-streamlit.write('Sizes Available: ',df2[2]) 
-streamlit.write(df2[3])
+streamlit.write('Price: ', df2[1]) streamlit.write('Sizes Available: ',df2[2]) streamlit.write(df2[3])

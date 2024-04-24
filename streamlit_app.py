98f7cff4bbf0fc06a6a8e2c7de2bda7ed888a649
Donavan <donbugan@gmail.com>
2024-04-24 14:13:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,19 @@ import streamlit
 import snowflake.connector 
 import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
+
 # connect to snowflake
-my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(
+    user=streamlit.secrets["snowflake"]["user"],
+    password=streamlit.secrets["snowflake"]["password"],
+    account=streamlit.secrets["snowflake"]["account"],
+    warehouse=streamlit.secrets["snowflake"]["warehouse"],
+    database=streamlit.secrets["snowflake"]["database"],
+    schema=streamlit.secrets["snowflake"]["schema"],
+    role=streamlit.secrets["snowflake"]["role"],
+    client_session_keep_alive=streamlit.secrets["snowflake"]["client_session_keep_alive"]
+)
+
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog my_cur.execute("select color_or_style from catalog_for_website") my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
